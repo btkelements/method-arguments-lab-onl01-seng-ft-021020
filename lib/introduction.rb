70271744ace729fba50751987dd87907642a1ab7
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-13 01:04:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-#intro with language
+#introduction_with_languag
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
